094efce1a7d37334ec6972d037be741e8de73c9e
al-burlachenko <alexandrburl@gmail.com>
2025-10-25 21:55:23 +0300

    adds trained with QueryClient, QueryClientProvider, useQuery

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+// QueryClient це об’єкт, який містить конфігурацію й управління запитами та кешуванням даних.
+// QueryClientProvider надає всі можливості React Query для компонентів всередині нього, дозволяючи працювати з асинхронними запитами, кешем і відслідковувати їхній стан.
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>,
-)
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+);
